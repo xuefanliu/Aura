863658201e66adbe69cad6ad0380e9f324c73842
xuefanliu <2895969941@qq.com>
2024-08-07 18:28:35 +0800

    Create Character Class and Enemy Class

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Liu Xuefan
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Liu Xuefan
+
+
+#include "Character/AuraEnemy.h"
+
