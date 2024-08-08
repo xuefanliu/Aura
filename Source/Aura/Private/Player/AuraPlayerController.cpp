21473b9bf4078b011b45b524e743da4d391f5e08
xuefanliu <2895969941@qq.com>
2024-08-09 02:08:20 +0800

    EnemyInterface and Highlight Enemy Under Cursor

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,12 +4,79 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
+
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;  // 当当前实体在服务器上发生变化时，变化将被复制和发送给其他客户端
 	
 }
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+	CursorTrace();   // 处理指针高亮
+
+	
+}
+
+
+
+
+void AAuraPlayerController::CursorTrace()
+{
+
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/**
+	 *  1. LastActor = null && ThisActor = null
+	 *		- Do nothing
+	 *	2. LastActor = null && ThisActor != null
+	 *		- HightLight ThisActor
+	 *  3. LastActor != null && ThisActor = null
+	 *		- UnHighLight LastActor
+	 *	4. LastActor != null && ThisActor != null
+	 *		- LastActor == ThisActor:
+	 *			- Do nothing
+	 *		- LastActor != ThisActor:
+	 *			- HightLight ThisActor
+	 *			- UnHighLight LasyActor
+	 **/
+
+	if(LastActor == nullptr)
+	{
+		if(ThisActor == nullptr)
+		{
+			// Do nothing
+		}else
+		{
+			ThisActor->HighlightActor();
+		}
+	}else
+	{
+		if(ThisActor == nullptr)
+		{
+			LastActor->UnHighlightActor();
+		}else
+		{
+			if(ThisActor == LastActor)
+			{
+				// Do nothing
+			}else
+			{
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+		}
+	}
+}
+
+
+
 
 void AAuraPlayerController::BeginPlay()
 {
@@ -39,6 +106,9 @@ void AAuraPlayerController::SetupInputComponent()
 	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
 }
 
+
+
+
 void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 {
 	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
@@ -54,3 +124,5 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
+
+
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class IEnemyInterface;
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
@@ -16,11 +17,17 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
+
+
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
+	
 
 private:
 
@@ -31,4 +38,8 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const struct FInputActionValue& InputActionValue);
+	void CursorTrace();
+
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
 };
